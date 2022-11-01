d9d7b39265bf1a1de0da01f33dff99b706a51777
Daisigu <bogd228@mail.ru>
2022-11-01 15:00:26 +0700

    123123

---
+++ src/components/car-catalog/v-catalog-filter.vue
@@ -0,0 +1,100 @@
+<template>
+    <div>
+        <form>
+            <div class="mb-2">
+                <label for="">Тип ТС</label>
+                <select name="" id="" class="form-select" v-model="carType">
+                    <option value="0">
+                        Легковые автомобили
+                    </option>
+                    <option value="0">
+                        Легковые автомобили
+                    </option>
+                </select>
+            </div>
+
+            <div class="form-check mb-2">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" v-model="carAviabillity">
+                <label class="form-check-label" for="flexCheckChecked">Автомобили в наличии</label>
+            </div>
+            <div class="mb-2">
+                <label for="">Марка</label>
+                <select name="" id="" class="form-select" v-model="carMark">
+                    <option value="0">
+                        Легковые автомобили
+                    </option>
+                    <option value="0">
+                        Легковые автомобили
+                    </option>
+
+                </select>
+            </div>
+            <div class="mb-2">
+                <label for="">Модель</label>
+                <select name="" id="" class="form-select" v-model="carModel">
+                    <option value="0">
+                        Легковые автомобили
+                    </option>
+                    <option value="0">
+                        Легковые автомобили
+                    </option>
+
+                </select>
+            </div>
+            <div class="mb-2">
+                <label for="">Комплектация</label>
+                <select name="" id="" class="form-select" v-model="carComplictation">
+                    <option value="0">
+                        Легковые автомобили
+                    </option>
+                    <option value="0">
+                        Легковые автомобили
+                    </option>
+
+                </select>
+            </div>
+            <div class="d-flex flex-column">
+                <button type="button" class="btn btn-primary mb-2" @click="postFilter()">Показать</button>
+                <p class="text-center clearFilters" @click="clearFilters()">Очистить все</p>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            carType: '',
+            carAviabillity: '',
+            carMark: '',
+            carModel: '',
+            carComplictation: '',
+        }
+    },
+    methods: {
+        postFilter(){
+            console.log(
+            this.carType,
+            this.carAviabillity,
+            this.carMark,
+            this.carModel,
+            this.carComplictation
+            );
+        },
+        clearFilters(){
+            this.carType= ''
+            this.carAviabillity= ''
+            this.carMark= ''
+            this.carModel= ''
+            this.carComplictation= ''
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.clearFilters{
+    cursor: pointer;
+}
+</style>
--- src/views/CatalogView.vue
+++ src/views/CatalogView.vue
@@ -1,122 +1,28 @@
 <template>
-    <div class="container mt-5">
+    <div id="container" class="mt-5">
         <h1>Каталог автомобилей</h1>
-        <!--  <div class="d-flex flex-wrap justify-content-around">
-            <router-link :to="'/car/' + car._id" class="car-card mb-3 shadow-sm card" style="width: 18rem;"
-                v-for="car in cars" :key="car._id">
-                <img :src="'http://localhost:5000/' + car.photo" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">{{ car.mark }} {{ car.model }}</h5>
-                    <p class="card-text">{{ Number(car.price).toLocaleString('ru-RU') }} Руб.</p>
-                </div>
-            </router-link>
-        </div> -->
-        <div class="d-flex flex-column">
-            <router-link :to="'/car/' + car._id" class="car-card col-12 d-flex flex-row" v-for="car in cars"
-                :key="car._id">
-                <img :src="'http://localhost:5000/' + car.photo" class="car-img" alt="...">
-                <div class="w-100">
-                    <h5 class="car-title">{{ car.mark }} {{ car.model }}</h5>
-                    <div class="d-flex flex-row col-12 justify-content-between">
-                        <div class="d-flex flex-row">
-                            <div class="me-5">
-                                <div>
-                                    <span class="description-text-left">Год выпуска:</span> <span
-                                        class="description-text-right"> {{ car.year }}</span>
-                                </div>
-                                <div>
-                                    <span class="description-text-left">Двигатель:</span> <span
-                                        class="description-text-right">{{ car.engine }}</span>
-                                </div>
-                                <div>
-                                    <span class="description-text-left">Наличие:</span> <span
-                                        class="description-text-right">{{ car.availability }}</span>
-                                </div>
-                            </div>
-                            <div>
-                                <div>
-                                    <span class="description-text-left">Трансмиссия:</span> <span
-                                        class="description-text-right">{{ car.transmission }}</span>
-                                </div>
-                                <div>
-                                    <span class="description-text-left">Кузов:</span> <span
-                                        class="description-text-right">{{ car.body }}</span>
-                                </div>
-                                <div>
-                                    <span class="description-text-left">Цвет:</span> <span
-                                        class="description-text-right">{{ car.color }}</span>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-2 d-flex flex-column">
-                            <h5 class="price align-self-start">{{ Number(car.price).toLocaleString('ru-RU') }} руб.</h5>
-                            <p>Платеж от {{ ((car.price / 74).toFixed(0)).toLocaleString('ru-RU') }} руб.</p>
-                        </div>
-                    </div>
-
-                </div>
-            </router-link>
-        </div>
+      <div class="row">
+        <v-car-catalog class="col-10"></v-car-catalog>
+        <v-catalog-filter class="col-2"></v-catalog-filter>
+      </div>
     </div>
 </template>
 
 <script>
-import { mapMutations, mapState } from 'vuex';
+import vCarCatalog from '@/components/car-catalog/v-car-catalog.vue';
+import vCatalogFilter from '@/components/car-catalog/v-catalog-filter.vue';
 export default {
-    computed: {
-        ...mapState([
-            'cars'
-        ])
-    },
-    methods: {
-        ...mapMutations([
-            'getCurrentCar',
-            'getAllCars'
-        ])
-    },
-    mounted() {
-        this.getAllCars()
+    components: {
+        vCarCatalog, vCatalogFilter
     }
 }
 </script>
 
 <style scoped>
-.car-card {
-    height: 150px;
-    padding: 5px;
-    padding-bottom: 10px;
-    padding-top: 10px;
-    margin-bottom: 5px;
-    border-bottom: 1px solid rgb(204, 204, 204);
-}
-
-.car-card:hover .car-title {
-    color: coral;
-   
-}
-.car-card:hover{
-    opacity: 0.8;
-    cursor: pointer;
-    transition: all 0.3s ease;
-}
-
-.car-img {
-    border-radius: 5px;
-    margin-right: 5px;
-    width: 200px;
-}
-
-.price {
-    padding: 5px;
-    background: rgb(236, 236, 236);
-}
-
-.description-text-right {
-    color: black;
-    font-weight: 500;
-}
-
-.description-text-left {
-    color: gray;
+#container {
+    position: relative;
+    width: 80%;
+    margin-left: auto;
+    margin-right: auto;
 }
 </style>
