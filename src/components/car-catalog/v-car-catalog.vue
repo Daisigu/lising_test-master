3c9be596ebff3a96ee2cec4c26dc6499368ef7d6
Daisigu <bogd228@mail.ru>
2022-11-02 12:28:48 +0700

    124

--- src/components/car-catalog/v-car-catalog.vue
+++ src/components/car-catalog/v-car-catalog.vue
@@ -1,80 +1,105 @@
 <template>
     <div class="d-flex flex-column">
-        <button @click="getLimitCars(page);">test</button>
-            <router-link :to="'/car/' + car._id" class="car-card col-12 d-flex flex-row" v-for="car in cars"
-                :key="car._id">
-                <img :src="'http://localhost:5000/' + car.photo" class="car-img" alt="...">
-                <div class="w-100">
-                    <h5 class="car-title">{{ car.mark }} {{ car.model }}</h5>
-                    <div class="d-flex flex-row col-12 justify-content-between">
-                        <div class="d-flex flex-row">
-                            <div class="me-5">
-                                <div>
-                                    <span class="description-text-left">Год выпуска:</span> <span
-                                        class="description-text-right"> {{ car.year }}</span>
-                                </div>
-                                <div>
-                                    <span class="description-text-left">Двигатель:</span> <span
-                                        class="description-text-right">{{ car.engine }}</span>
-                                </div>
-                                <div>
-                                    <span class="description-text-left">Наличие:</span> <span
-                                        class="description-text-right">{{ car.availability }}</span>
-                                </div>
+        <router-link :to="'/car/' + car._id" class="car-card col-12 d-flex flex-row" v-for="car in cars" :key="car._id">
+            <img :src="'http://localhost:5000/' + car.photo" class="car-img" alt="...">
+            <div class="w-100">
+                <h5 class="car-title">{{ car.mark }} {{ car.model }}</h5>
+                <div class="d-flex flex-row col-12 justify-content-between">
+                    <div class="d-flex flex-row">
+                        <div class="me-5">
+                            <div>
+                                <span class="description-text-left">Год выпуска:</span> <span
+                                    class="description-text-right"> {{ car.year }}</span>
                             </div>
                             <div>
-                                <div>
-                                    <span class="description-text-left">Трансмиссия:</span> <span
-                                        class="description-text-right">{{ car.transmission }}</span>
-                                </div>
-                                <div>
-                                    <span class="description-text-left">Кузов:</span> <span
-                                        class="description-text-right">{{ car.body }}</span>
-                                </div>
-                                <div>
-                                    <span class="description-text-left">Цвет:</span> <span
-                                        class="description-text-right">{{ car.color }}</span>
-                                </div>
+                                <span class="description-text-left">Двигатель:</span> <span
+                                    class="description-text-right">{{ car.engine }}</span>
+                            </div>
+                            <div>
+                                <span class="description-text-left">Наличие:</span> <span
+                                    class="description-text-right">{{ car.availability }}</span>
                             </div>
                         </div>
-                        <div class="col-2 d-flex flex-column">
-                            <h5 class="price align-self-start">{{ Number(car.price).toLocaleString('ru-RU') }} руб.</h5>
-                            <p>Платеж от {{ ((car.price / 74).toFixed(0)).toLocaleString('ru-RU') }} руб.</p>
+                        <div>
+                            <div>
+                                <span class="description-text-left">Трансмиссия:</span> <span
+                                    class="description-text-right">{{ car.transmission }}</span>
+                            </div>
+                            <div>
+                                <span class="description-text-left">Кузов:</span> <span
+                                    class="description-text-right">{{ car.body }}</span>
+                            </div>
+                            <div>
+                                <span class="description-text-left">Цвет:</span> <span class="description-text-right">{{
+                                        car.color
+                                }}</span>
+                            </div>
                         </div>
                     </div>
-
+                    <div class="col-2 d-flex flex-column">
+                        <h5 class="price align-self-start">{{ Number(car.price).toLocaleString('ru-RU') }} руб.</h5>
+                        <p>Платеж от {{ ((car.price / 74).toFixed(0)).toLocaleString('ru-RU') }} руб.</p>
+                    </div>
                 </div>
-            </router-link>
-            <div style="width: 100%; background-color:green; height: 50px;" ref="observer" ></div>
-        </div>
+
+            </div>
+        </router-link>
+        <div ref="observer"  ></div>
+    </div>
 </template>
 
 <script>
-  import { mapMutations, mapState } from 'vuex';
-  import axios from 'axios';
+import { mapMutations, mapState } from 'vuex';
+
+
+
+
+import axios from 'axios';
 export default {
-    data(){
+    data() {
         return {
             page: 0,
             cars: [],
         }
     },
+    components: {
+    },
     computed: {
     },
     methods: {
+        
         ...mapMutations([
             'getCurrentCar',
             'getAllCars'
         ]),
-        getLimitCars(page){
-            axios.post("http://localhost:5000/cars/getLimit?page="+page).then((res) => {
-            this.cars = [...this.cars, ...res.data];
-      })
-      this.page++
+        getLimitCars(page) {
+            axios.post("http://localhost:5000/cars/getLimit?page=" + page).then((res) => {
+                this.cars = [...this.cars, ...res.data];
+            })
+            this.page++
         }
     },
     mounted() {
         this.getLimitCars(this.page)
+        
+
+
+
+        const options = {
+            rootMargin: '0px',
+            threshold: 1.0
+        }
+        const callback = (entries, observer)=>{
+           if (entries[0].isIntersecting){
+            this.getLimitCars(this.page)
+           }
+        };
+        const observer = new IntersectionObserver(callback, options)
+        observer.observe(this.$refs.observer);
+
+
+
+
     }
 }
 </script>
@@ -91,9 +116,10 @@ export default {
 
 .car-card:hover .car-title {
     color: coral;
-   
+
 }
-.car-card:hover{
+
+.car-card:hover {
     opacity: 0.8;
     cursor: pointer;
     transition: all 0.3s ease;
--- src/components/v-spinner.vue
+++ src/components/v-spinner.vue
@@ -1,19 +1,22 @@
 <template>
     <div class="spinner-fade">
-        <div class="spinner-border" role="status" style="width: 18rem; height: 18rem;">
-  <span class="visually-hidden">Loading...</span>
-</div>
+        <div class="spinner-border" role="status" :style="'width: ' + w + 'rem; height: ' + h + 'rem;'">
+            <span class="visually-hidden">Loading...</span>
+        </div>
     </div>
 </template>
 
 <script>
-    export default {
-        
+export default {
+    props: {
+        h: Number,
+        w: Number
     }
+}
 </script>
 
 <style scoped>
-.spinner-fade{
+.spinner-fade {
     position: fixed;
     top: 56px;
     display: flex;
@@ -21,7 +24,7 @@
     align-items: center;
     width: 100vw;
     height: 100vh;
-    background-color: rgba(255, 255, 255, 0.856) ;
+    background-color: rgba(255, 255, 255, 0.856);
     left: 0;
     z-index: 2;
 }
