2b7ce9e29f70ada7afdb9c43a248e758d5efd109
Daisigu <bogd228@mail.ru>
2022-11-27 15:41:35 +0700

    123

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <v-navbar></v-navbar>
-  <router-view :key="$route.fullPath"></router-view>
+  <router-view v-slot="{ Component }">
+  <keep-alive>
+    <component :is="Component" />
+  </keep-alive>
+</router-view>
 </template>
 
 <script>
